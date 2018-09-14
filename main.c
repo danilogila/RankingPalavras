6bd5fa5a130be6bc33bd380707823ccbb794e8c4
jlgsobreiro <jlgsobreiro@gmail.com>
2018-09-14 09:43:05 -0300

    Lendo arquivo txt
    Codigo no para ler texto em aquivo.
    Codigo localizado no main.c

--- main.c
+++ main.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "Mapa.h"
 
 int main()
 {
-    printf("Hello world!\n");
+    int i = 0;
+    FILE *fp;
+    char *str = calloc(1000, sizeof(char));
+    char* filename = "lorem.txt";
+
+    fp = fopen(filename, "r");
+    if (fp == NULL){
+        printf("Could not open file %s",filename);
+        return 1;
+    }
+    while (fgets(str, 1000, fp) != NULL){
+        i++;
+
+    }
+    fclose(fp);
+
+    for(i = 0; i < 600; i++){
+        if(!strcmp(&str[i], " ")){
+            strcpy(&str[i] , ";");
+        }
+        if(!strcmp(&str[i], ".")){
+                strcpy(&str[i] , "");
+        }
+        if(!strcmp(&str[i], ",")){
+                strcpy(&str[i] , "");
+        }
+    }
+printf("%s", str);
     return 0;
 }
